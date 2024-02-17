fee3306381a32ce12045f80faad93c0619970409
username <cptchen@ucdavis.edu>
2024-02-17 13:55:07 -0800

    new

--- 55colorname.py
+++ 55colorname.py
@@ -4,15 +4,16 @@ def dtc(P, Q):
     d = 0
     for p, q in zip(P, Q):
         d += abs(p - q)
-    return d
+    return d #Oh, busted! Caught red-handed, I see. I must say, I applaud...
 
 colorfile = sys.argv[1]
 input_RGB = [int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])]
-file_RGB = [0, 0, 0]
+#This crap has been moved to line16
 distance = 99999
 
 with open(colorfile) as fp:
 	for line in fp:
+		file_RGB = [0, 0, 0]
 		words = line.split()
 		elements = words[2].split(',')
 		for i in range(3):
